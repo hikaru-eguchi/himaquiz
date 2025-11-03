0d933da44f267ae015a7b1bea01737ed7116bef8
hikarueguchi <hikaru.e0220hirokazu@gmail.com>
2025-11-03 17:42:10 +0900

    記事修正

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -5,6 +5,7 @@ import { remark } from 'remark';
 import html from 'remark-html';
 import remarkGfm from 'remark-gfm';
 import type { Metadata } from 'next';
+import Breadcrumb from "@/components/Breadcrumb";
 
 interface ArticleData {
   id: string;
